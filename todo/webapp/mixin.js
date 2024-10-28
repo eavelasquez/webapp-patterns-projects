8a7bd9d8de00497e3f2e04c22711bb87c605cf3a
Esteban Velasquez <estebanvelasquezcardeno@gmail.com>
2024-10-28 07:30:21 -0500

    feat: add mixin that implement observer pattern

---
+++ todo/webapp/mixin.js
@@ -0,0 +1,13 @@
+// Mixin that will implement Observer pattern
+export const observerMixin = {
+  observers: new Set(),
+  addObserver: (observer) => {
+    this.observers.add(observer)
+  },
+  removeObserver: (observer) => {
+    this.observers.delete(observer)
+  },
+  notify: () => {
+    this.observers.forEach((observer) => observer());
+  }
+}
