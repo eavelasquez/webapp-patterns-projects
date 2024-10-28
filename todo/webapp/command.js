6db946073523f9fb6a9a99869a32602f6e090dcf
Esteban Velasquez <estebanvelasquezcardeno@gmail.com>
2024-10-28 08:22:17 -0500

    feat: add command file that implement command pattern

---
+++ todo/webapp/command.js
@@ -0,0 +1,40 @@
+import { TodoItem, TodoList } from './classes.js'
+
+export class Command {
+  name
+  args // Array
+
+  constructor(name, args) {
+    this.name = name
+    this.args = args
+  }
+}
+
+export const Commands = {
+  ADD: 'add',
+  DELETE: 'delete'
+}
+
+// Singleton pattern as an object literal
+export const CommandExecutor = {
+  execute(command) {
+    const todoList = TodoList.getInstance()
+    switch (command.name) {
+      case Commands.ADD:
+        const todoInput = globalThis.DOM.todoInput
+        const todoText = todoInput.value.trim()
+        const itemInList = todoList.find(todoText)
+
+        if (todoText === '' && itemInList !== undefined) return
+
+        todoInput.value = ''
+        const itemToAdd = new TodoItem(todoText)
+        todoList.add(itemToAdd)
+        break
+      case Commands.DELETE:
+        const [textToDelete] = command.args
+        todoList.delete(textToDelete)
+        break
+    }
+  }
+}
