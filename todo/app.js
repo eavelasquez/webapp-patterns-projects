48457fed58b9d9e7df568d8a08f97006ebb18df9
Esteban Velasquez <estebanvelasquezcardeno@gmail.com>
2024-10-28 08:22:54 -0500

    feat: implement app logic using command and add renderList function

--- todo/app.js
+++ todo/app.js
@@ -1,5 +1,23 @@
+import { TodoList } from './webapp/classes.js'
+import { Command, CommandExecutor, Commands } from './webapp/command.js'
+
 globalThis.DOM = {}
 const DOM = globalThis.DOM
+
+function renderList() {
+    DOM.todoList.innerHTML = ''
+    const list = TodoList.getInstance()
+    for (const todo of list.items) {
+        const listItem = document.createElement('li')
+        listItem.classList.add('todo-item')
+        listItem.innerHTML = `
+            ${todo.text}
+            <button class='delete-btn'>Delete</button>
+        `
+        listItem.dataset.text = todo.text
+        DOM.todoList.appendChild(listItem)
+    }
+}
 
 document.addEventListener('DOMContentLoaded', () => {
     DOM.todoList = document.getElementById('todo-list')
@@ -7,12 +25,17 @@ document.addEventListener('DOMContentLoaded', () => {
     DOM.todoInput = document.getElementById('todo-input')
 
     DOM.addBtn.addEventListener('click', (event) => {
-
+        const cmd = new Command(Commands.ADD)
+        CommandExecutor.execute(cmd)
     })
 
     DOM.todoList.addEventListener('click', (event) => {
         if (event.target.classList.contains('delete-btn')) {
-
+            const todo = event.target.parentNode.dataset.text
+            const cmd = new Command(Commands.DELETE, [todo])
+            CommandExecutor.execute(cmd)
         }
     })
+
+    TodoList.getInstance().addObserver(renderList)
 })
